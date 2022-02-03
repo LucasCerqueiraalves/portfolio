5ca52e6090e324cec7ae0fdc0ad37ddf28d7d5e8
Lucas Cerqueira <lucascerqueiraalves0@gmail.com>
2022-02-03 19:41:45 -0300

    Portfolio

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    
+    <title>Document</title>
+</head>
+<body>
+    <!--HEADER-->
+    <header class="l-header">
+        <nav class="nav bd-grid">
+            <div>
+                <a href="#" class="nav__logo">Lucas Cerqueira</a>
+            </div>
+            <div class="nav__menu" id="nav-menu">
+                <ul class="nav__list">
+                    <li class="nav__item"><a href="#home" class="nav__link active">Home</a></li>
+                    <li class="nav__item"><a href="#sobre" class="nav__link">Sobre</a></li>
+                    <li class="nav__item"><a href="#habilidade" class="nav__link">Habilidades</a></li>
+                    <li class="nav__item"><a href="#projetos" class="nav__link">Projetos</a></li>
+                    <li class="nav__item"><a href="#contato" class="nav__link">Contato</a></li>
+                </ul>
+            </div>
+
+            <div class="nav__toggle" id="nav-toggle">
+                <i class="bx bx-menu"></i>
+            </div>
+        </nav>
+    </header>
+
+    <main class="l-main">
+        <!--HOME-->
+        <section class="home bd-grid" id="home">
+            <div class="home__data">
+                <h1 class="home__title">
+                    Oi Eu sou  <br/><span class="home__title-color">Lucas Cerqueira</span><br/>
+                    Desenvolvedor<br/> Front-End JR.
+                </h1>
+
+                <a href="./assets/curriculo/Currículo Profissional em Preto.pdf" class="button" download="curriculo.pdf" type="application/pdf">Abaixar CV</a>
+            </div>
+
+            <div class="home__social">
+                <a href="" class="home__social-icon"><i class='bx bxl-linkedin'></i></a>
+                <a href="" class="home__social-icon"><i class='bx bxl-github'></i></a>
+                <a href="" class="home__social-icon"><i class='bx bxl-gmail'></i></a>
+            </div>
+
+            <div class="home__img">
+                <div class="bgColor"></div>
+                <img src="assets/img/perfil.png" alt="Foto de Perfil" />
+            </div>
+
+        </section>
+        
+        <!--SOBRE-->
+            <section class="sobre section" id="sobre">
+                <h2 class="section-title">Sobre</h2>
+
+                <div class="about__container bd-grid">
+                    <div class="about__img">
+                        <img src="assets/img/about.jpg" alt="Foto sobre" />
+                    </div>
+
+                    <div>
+                        <h2 class="about__subtitle">Eu Sou <span class="subitle">Lucas Cerqueira</span></h2>
+                        <p class="about__text"></p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse libero quae aliquid vero molestias nulla rem, illo obcaecati cupiditate eum pariatur molestiae ipsa dignissimos repudiandae, at sequi? Eligendi, deserunt optio.</p>
+                    </div>
+                </div>
+
+            </section>
+        <!--HABILIDADES-->
+            <section class="skills section" id="habilidade">
+                <h2 class="section-title">Habilidades</h2>
+
+                <div class="skills__container bd-grid">
+                    <div>
+                        <h2 class="skills__subtitle">Habilidades Profissionais</h2>
+                        <p class="skills__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sequi odio a repellendus reiciendis minima possimus vel earum </p>
+
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bxl-html5 skills__icon'></i>
+                                <span class="skills__name">HTML5</span>
+                            </div>
+
+                            <div>
+                                <span class="skills__percentage">95%</span>
+                            </div>
+
+                            <div class="skills__bar skills__html"></div>
+                        </div>
+
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bxl-css3 skills__icon'></i>
+                                <span class="skills__name">CSS</span>
+                            </div>
+
+                            <div>
+                                <span class="skills__percentage">95%</span>
+                            </div>
+
+                            <div class="skills__bar skills__css"></div>
+                        </div>
+
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bxl-javascript skills__icon'></i>
+                                <span class="skills__name">Javascript</span>
+                            </div>
+
+                            <div>
+                                <span class="skills__percentage">80%</span>
+                            </div>
+
+                            <div class="skills__bar skills__js"></div>
+                        </div>
+
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bxl-react skills__icon'></i>
+                                <span class="skills__name">ReactJs</span>
+                            </div>
+
+                            <div>
+                                <span class="skills__percentage">50%</span>
+                            </div>
+
+                            <div class="skills__bar skills__react"></div>
+                        </div>
+
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <i class='bx bxl-github skills__icon'></i>
+                                <span class="skills__name">GitHub</span>
+                            </div>
+
+                            <div>
+                                <span class="skills__percentage">89%</span>
+                            </div>
+
+                            <div class="skills__bar skills__git"></div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src="assets/img/work3.jpg" alt="trbalho" class="skills__img"/>
+                    </div>
+                </div>
+            </section>
+        <!--PROJETO-->
+            <section class="work section" id="projetos">
+                <h2 class="section-title">Projetos</h2>
+
+                <div class="work__container bd-grid">
+                    <div class="work__img">
+                        <img src="assets/img/work1.jpg" alt="">
+                        <p>titulo</p>
+                    </div>
+
+                    <div class="work__img">
+                        <img src="assets/img/work2.jpg" alt="">
+                        <p>titulo</p>
+                    </div>
+
+                    <div class="work__img">
+                        <img src="assets/img/work3.jpg" alt="">
+                        <p>titulo</p>
+                    </div>
+
+                    <div class="work__img">
+                        <img src="assets/img/work4.jpg" alt="">
+                        <p>titulo</p>
+                    </div>
+                </div>
+            </section>
+        <!--CONTATO-->
+            <section class="contact section" id="contato">
+                <h2 class="section-title bd-grid">Contato</h2>
+                    <div class="infoPessoal">
+                        <div class="iconTel">
+                            <i class='bx bxs-phone iconTel'></i>
+                            <p>(11) 98524-2687</p>
+                        </div>
+                    </div>
+                    
+                <div class="contact__container">
+                    <form method="POST" action="./assets/php/processa.php" name="contactform" class="contact__form">
+                        <input type="text" name="nome" placeholder="Nome completo" class="contact__input" />
+                        <input type="mail"  name="email" placeholder="Email" class="contact__input" />
+                        <textarea name="mensagem" id="" cols="0" rows="10" class="contact__input"></textarea>
+
+                        <input type="submit" value="Enviar" class="contact__button button" />
+                    </form>
+                </div>
+            </section>
+    </main>
+
+    <!--FOOTER-->
+    <footer class="footer">
+        <p class="footer__title">Lucas Cerqueira</p>
+
+        <div class="footer__social">
+            <a href="#" class="footer__icon"><i class='bx bxl-github'></i></a>
+            <a href="#" class="footer__icon"><i class='bx bxl-linkedin'></i></a>
+           
+        </div>
+        <p class="direito">&#169Lucas Cerqueira copyright all right reserved</p>
+    </footer>
+
+
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <script src="assets/js/script.js"></script>
+    
+</body>
+</html>
